f431e2a5db64ea4b89ba338cc2a1e27e045be2d4
Natasha Whitney <natiwhitney@gmail.com>
2020-01-21 15:31:40 -0500

    toggle between ls/pwd with switch statement

--- bash.js
+++ bash.js
@@ -1,7 +1,18 @@
 const pwdFnc = require("./pwd");
+const lsFnc = require("./ls");
 
-process.stdout.write("prompt > ");
+process.stdout.write("\nprompt > ");
 process.stdin.on("data", data => {
   const cmd = data.toString().trim();
-  pwdFnc(cmd);
+  switch (cmd) {
+    case "pwd":
+      pwdFnc();
+      break;
+    case "ls":
+      lsFnc();
+      break;
+    default:
+      process.stdout.write("\nprompt > ");
+      break;
+  }
 });
