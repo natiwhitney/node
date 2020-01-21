c9555f9878f76871a7450f597e5129e5e5147756
Natasha Whitney <natiwhitney@gmail.com>
2020-01-21 12:51:40 -0500

    Add pwd

--- bash.js
+++ bash.js
@@ -2,8 +2,9 @@ process.stdout.write("prompt > ");
 
 process.stdin.on("data", data => {
   const cmd = data.toString().trim();
-  process.stdout.write("You typed: " + cmd);
+  process.stdout.write("You typed: " + cmd + "\n");
+  if (cmd === "pwd") {
+    process.stdout.write(process.env.PWD);
+  }
   process.stdout.write("\nprompt > ");
 });
-
-random crap
